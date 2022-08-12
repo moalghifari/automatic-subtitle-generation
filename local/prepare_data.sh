45f3e036e8a6fe4f4caa3e9bb5ece71641f959f0
Mochamad Alghifari <mo.alghifari@gmail.com>
2022-08-12 20:55:58 +0700

    Fix dir

--- local/prepare_data.sh
+++ local/prepare_data.sh
@@ -8,11 +8,11 @@ cd local/
 
 cut -d ' ' -f 2- ../data/$folder/text | sed 's/ /\n/g' | sort -u > ../data/$folder/words.txt
 
-python create_path_file.py $folder
+python utils/create_path_file.py $folder
 
-python create_uttr_to_speaker.py $folder
+python utils/create_uttr_to_speaker.py $folder
 
-python create_speaker_to_uttr.py $folder
+python utils/create_speaker_to_uttr.py $folder
 
 # for x in $train_folder $test_folder; do 
 #     python create_speaker_to_gender.py data/$folder
