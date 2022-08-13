0bf559cac76cf3e23ede52a4190f6528b068d61f
Mochamad Alghifari <mo.alghifari@gmail.com>
2022-08-13 13:16:36 +0700

    Add env info

--- commons.py
+++ commons.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 model_list = [
     'cnn_dbn-dnn_smbr',
     'dbn_dnn',
     'tri_delta',
-    'tri_lda_mllt_sat'
+    'tri_sat_fmllr'
 ]
 transcriptions_path = {
     'cnn_dbn-dnn_smbr': 'transcriptions/cnn_dbn-dnn_smbr',
     'dbn_dnn': 'transcriptions/dbn_dnn',
     'tri_delta': 'transcriptions/tri_delta',
-    'tri_lda_mllt_sat': 'transcriptions/tri_lda_mllt_sat'
+    'tri_sat_fmllr': 'transcriptions/tri_sat_fmllr'
 }
--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 from flask import jsonify
 from string import ascii_letters
